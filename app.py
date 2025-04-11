9250dfcae4c2389be4de7fa6a497cf39d7db9a55
Sala 203 <sala203@ujaverianas.loc>
2025-04-11 14:01:55 -0500

    hola

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+apgn = pd.read_csv('datos_anteproyecto.csv')
+ran = pd.read_csv('datos_random.csv')
+
+st.title("Aplicación 2")
+
+tab1, tab2 = st.tabs(['Tab 1', 'Tab 2'])
+
+
+with tab1:
+    #analisis univariado
+    fig, ax = plt.subplots(1, 3, figsize = (10, 4))
+
+    # educ
+    tab_freq = ran['educ'].value_counts().sort_index()
+    ax[0].bar(tab_freq.index, tab_freq.values)  # grafico de barras y siempre necesitare la tabla de frecuencias, que es el renglon anterior
+
+    # edad
+    ax[1].hist(ran['edad'], bins = 40) # histograma 
+
+    # wage
+    ax[2].hist(ran['wage'], bins = 40)
+
+    st.pyplot(fig)
+
+    # analisis bivariado
+    fig, ax = plt.subplots(1, 2, figsize = (10, 4)) # 1, 2 es 1 fila 2 columnas
+
+    # educ vs. wage
+    ax[0].scatter(ran['educ'], ran['wage']) # grafico de dispersión
+
+    # edad vs. wage
+    ax[1].scatter(ran['edad'], ran['wage'])
+
+    st.pyplot(fig)
+
+
+with tab2:
+   fig = px.treemap(data_frame=apgn, path=[px.Constant("PGN"), 'Nombre Sector', 'Tipo de gasto'], values='Valor')
+
+   st.plotly_chart(fig)
